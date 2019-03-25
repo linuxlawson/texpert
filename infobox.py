309a06747e2e77313dc33f39399f3b9996f8b70b
David Lawson <linuslagoo@yahoo.com>
2019-03-25 15:50:34 -0400

    readonly

--- infobox.py
+++ infobox.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # Info box 
+# David Lawson
 
 import os
 import sys
@@ -20,30 +21,36 @@ root.option_add("*Font", "TkDefaultFont 9")
 frame1 = Frame()
 frame1.pack(fill=X)
         
-label1 = Label(frame1, anchor=W, text="Title:", width=6)
+label1 = Label(frame1, anchor=W, text="Title: ", width=6)
 label1.pack(side=LEFT, padx=4, pady=4)           
        
 entry1 = Entry(frame1)
+entry1.insert('end', "Texpert ")
+entry1.config(state='readonly')
 entry1.pack(fill=X, padx=4, expand=True)
         
 # author
 frame2 = Frame()
 frame2.pack(fill=X)
         
-label2 = Label(frame2, anchor=W, text="Author:", width=6)
+label2 = Label(frame2, anchor=W, text="Author: ", width=6)
 label2.pack(side=LEFT, padx=4, pady=4)        
 
 entry2 = Entry(frame2)
+entry2.insert('end', "David Lawson")
+entry2.config(state='readonly')
 entry2.pack(fill=X, padx=4, expand=True)
    
 # review     
 frame3 = Frame()
 frame3.pack(fill=BOTH, expand=True)
         
-label3 = Label(frame3, anchor=W, text="Review:", width=6)
+label3 = Label(frame3, anchor=W, text="Review: ", width=6)
 label3.pack(side=LEFT, anchor=N, padx=4, pady=4)        
 
-txt = Text(frame3, height=10)
+txt = Text(frame3, height=10, wrap='word')
+txt.insert('end', "Not the best, not the worst.")
+txt.config(state='normal', padx=2)
 txt.pack(fill=BOTH, pady=4, padx=4, expand=True)
 
 
