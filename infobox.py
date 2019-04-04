f3f7fff0aa79d7c03068d3c2f4b48ef0e5d0c4f2
David Lawson <linuslagoo@yahoo.com>
2019-04-04 12:47:15 -0400

    readonly

--- infobox.py
+++ infobox.py
@@ -8,7 +8,6 @@ import time
 import datetime
 
 import Tkinter as tk
-from Tkinter import *
 
 
 # Main Window
@@ -18,47 +17,47 @@ root.title("Info")
 root.option_add("*Font", "TkDefaultFont 9")
 
 # title
-frame1 = Frame()
-frame1.pack(fill=X)
+frame1 = tk.Frame()
+frame1.pack(fill='x')
         
-label1 = Label(frame1, anchor=W, text="Title: ", width=6)
-label1.pack(side=LEFT, padx=4, pady=4)           
+label1 = tk.Label(frame1, anchor='w', text="Title: ", width=6)
+label1.pack(side='left', padx=4, pady=4)           
        
-entry1 = Entry(frame1)
+entry1 = tk.Entry(frame1)
 entry1.insert('end', "Texpert ")
 entry1.config(state='readonly')
-entry1.pack(fill=X, padx=4, expand=True)
+entry1.pack(fill='x', padx=4, expand=True)
         
 # author
-frame2 = Frame()
-frame2.pack(fill=X)
+frame2 = tk.Frame()
+frame2.pack(fill='x')
         
-label2 = Label(frame2, anchor=W, text="Author: ", width=6)
-label2.pack(side=LEFT, padx=4, pady=4)        
+label2 = tk.Label(frame2, anchor='w', text="Author: ", width=6)
+label2.pack(side='left', padx=4, pady=4)        
 
-entry2 = Entry(frame2)
+entry2 = tk.Entry(frame2)
 entry2.insert('end', "David Lawson")
 entry2.config(state='readonly')
-entry2.pack(fill=X, padx=4, expand=True)
+entry2.pack(fill='x', padx=4, expand=True)
    
 # review     
-frame3 = Frame()
-frame3.pack(fill=BOTH, expand=True)
+frame3 = tk.Frame()
+frame3.pack(fill='both', expand=True)
         
-label3 = Label(frame3, anchor=W, text="Review: ", width=6)
-label3.pack(side=LEFT, anchor=N, padx=4, pady=4)        
+label3 = tk.Label(frame3, anchor='w', text="Review: ", width=6)
+label3.pack(side='left', anchor='n', padx=4, pady=4)        
 
-txt = Text(frame3, height=10, wrap='word')
-txt.insert('end', "Not the best, not the worst.")
+txt = tk.Text(frame3, height=10, wrap='word')
+txt.insert('end', "")
 txt.config(state='normal', padx=2)
-txt.pack(fill=BOTH, pady=4, padx=4, expand=True)
+txt.pack(fill='both', pady=4, padx=4, expand=True)
 
 
 # buttons
-clearButton = Button(text="Clear", command=lambda: txt.delete(1.0, END))
+clearButton = tk.Button(text="Clear", command=lambda: txt.delete(1.0, 'end'))
 clearButton.pack(side='left', padx=64, pady=4)
             
-closeButton = Button(text="Close", command=root.destroy)
+closeButton = tk.Button(text="Close", command=root.destroy)
 closeButton.pack(side='right', padx=4, pady=4)
 
 root.mainloop()  
